2af9bd89698a384bcfd408715880efc31eaafbf8
Peter <peter14585@gmail.com>
2020-05-24 17:51:03 +1000

    stripe and modal

--- src/components/HeroHomePage.js
+++ src/components/HeroHomePage.js
@@ -2,40 +2,49 @@ import React from 'react';
 import EventListFilters from './EventListFilters';
 import { NavLink } from 'react-router-dom';
 import EventImageSlider from './EventImageSlider';
-import PublicEventList from './PublicEventList';
+import PublicEventsList from './publicEvents/PublicEventsList';
 import { Container, Row, Col } from 'reactstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 
 const EventHomePage = () => (
-
-    
-    <div >
-        <Container className="themed-container" fluid={true}>
-            <Row className="home">
-                <Col xs="8" ><h1 className="page-title">Event Stop</h1></Col>
+    <div>
+        <Navbar collapseOnSelect expand="lg" className="header-bar_hero">
+                <Navbar.Brand bsPrefix="header-bar_logo" >Event Stop</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link><NavLink to="/signin" className="header-bar_hero_links">Log In</NavLink></Nav.Link>
+                        <Nav.Link><NavLink to="/create_account" className="header-bar_hero_links">Sign Up</NavLink></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+        </Navbar>
+        <Container className="themed-container hero-layout" fluid={true}>
+            {/* <Row className="home">
+                <Col xs="8" >
+                    <h1 className="page-title">Event Stop</h1>
+                </Col>
                 <Col xs="4" className="home__spacing">
-                    
                     <NavLink to="/signin"><button className="button">Log In</button></NavLink>
                     <NavLink to="/create_account" ><button className="button">Sign Up</button></NavLink>
-                    
                 </Col>
-            </Row>
+            </Row> */}
             <div className="hero-layout">
-            <Row>
-                <Col xs="4">
-                    <h1 className="image-slider__herotext">Schedule events and find public events to attend</h1>
-                    <p className="image-slider__herotext__underneath">Choose a day, choose a time and make it public or private.</p>
-                </Col>
-                <Col xs="8">
-                
-                <div className = 'image-slider'><EventImageSlider /></div>
-                
-                </Col>
-            </Row>
+                <Row>
+                    <Col xs="4">
+                        <h1 className="image-slider__herotext">Schedule events and find public events to attend</h1>
+                        <p className="image-slider__herotext__underneath">Choose a day, choose a time and make it public or private.</p>
+                    </Col>
+                    <Col xs="8">
+                        <div className = 'image-slider'>
+                            <EventImageSlider />
+                        </div>
+                    </Col>
+                </Row>
             </div>
             <Row>
-                <Col xs="12"><EventListFilters /></Col>
-                <Col xs="12"><PublicEventList /></Col>
+                <Col xl={{size: 11, offset: 1}}><EventListFilters /></Col>
+                <Col xs="12"><PublicEventsList /></Col>
             </Row>
         </Container>
     </div>
--- src/components/dashboard/publicPrivateStat.js
+++ src/components/dashboard/publicPrivateStat.js
@@ -19,40 +19,33 @@ export const publicPrivateStat = ({ publicEventCount, eventCount }) => {
     };
 
     const barData = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
         datasets: [
-                {
-                label: 'Tickets Sold',
-                backgroundColor: 'rgba(255,99,132,0.2)',
-                borderColor: 'rgba(255,99,132,1)',
-                borderWidth: 1,
-                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                hoverBorderColor: 'rgba(255,99,132,1)',
-                data: [65, 59, 80, 81, 56, 55, 40]
-                }
-            ]
+                    {
+                    label: 'Tickets Sold',
+                    backgroundColor: 'rgba(168, 208, 230,0.2)',
+                    borderColor: 'rgba(168, 208, 230,1)',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'rgba(168, 208, 230,0.4)',
+                    hoverBorderColor: 'rgba(168, 208, 230,1)',
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                    }
+                ]
     };
     
     return (
         <div>
-        <Row>
-            <Col xs="12" lg="6">
-            <Doughnut 
-                data={doughnutData}  
-            />
-            </Col>
-            <Col xs="12" lg="6">
-            <Bar 
-                data={barData}
-            />
-            </Col>
-        </Row>
             <Row>
-            
-            {/* <Col s={{ size: '7', order: 1 }}>
-                <h1 className="filter-text-summary">You have {publicEventCount} Public {eventWord} scheduled coming up.</h1>
-                <h1 className="filter-text-summary">You have {eventCount - publicEventCount} Private events scheduled coming up.</h1>
-            </Col> */}
+                <Col xs="12" lg="6">
+                    <Doughnut 
+                        data={doughnutData}  
+                    />
+                </Col>
+                <Col xs="12" lg="6">
+                    <Bar 
+                        data={barData}
+                    />
+                </Col>
             </Row>
         </div>
     );
