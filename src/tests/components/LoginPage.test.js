870b2c5128e707cfdc63187404768b4848a25a67
Peter <peter14585@gmail.com>
2020-05-28 18:16:52 +1000

    complete

--- src/tests/components/LoginPage.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { LoginPage } from '../../components/LoginPage'
-import { shallow } from 'enzyme';
-
-test('should correctly render LoginPage', () => {
-    const wrapper = shallow(<LoginPage />);
-    expect(wrapper).toMatchSnapshot();
-});
